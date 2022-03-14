d32d287893d1ec8176f90496e740b9a261f49bba
phil-grayson <>
2022-03-14 11:39:20 -0400

    updated draft of tumor-normal

--- dockerfiles/somatic_sentieon/somatic_sentieon_tumor_normal.sh
+++ dockerfiles/somatic_sentieon/somatic_sentieon_tumor_normal.sh
@@ -26,33 +26,26 @@ nt=$(nproc) #number of threads to use in computation, set to number of cores in 
 
 # need to have unique readgroups for the tumor and somatic samples
 
-samtools view -H $tumor_bam | grep "@RG" | awk '{print $2}' | awk 'BEGIN { FS = ":" } ; { print $2 }' > tumor_read_groups.txt
-samtools view -H $normal_bam | grep "@RG" | awk '{print $2}' | awk 'BEGIN { FS = ":" } ; { print $2 }' > normal_read_groups.txt
+samtools view -H $tumor_bam | grep "@RG" | awk '{print $2}' | awk 'BEGIN { FS = ":" } ; { print $2 }' > tumor_read_groups.txt || exit 1
+samtools view -H $normal_bam | grep "@RG" | awk '{print $2}' | awk 'BEGIN { FS = ":" } ; { print $2 }' > normal_read_groups.txt || exit 1
 
-#possible that read groups match between tumor and normal, and for TNscope they need to be unique. in the case that they do match (then statement) we replace everything with something generic ()
+#possible that read groups match between tumor and normal, and for TNscope they need to be unique. in the case that they do match ('then' statement) we replace everything with something generic (https://support.sentieon.com/manual/examples/examples/?highlight=tnscope#modify-rg-information-on-bam-files-when-both-tumor-and-normal-inputs-have-the-same-rgid), otherwise, we use the existing read groups
 if [ $(grep -f tumor_read_groups.txt normal_read_groups.txt | wc -l) -gt 0 ]
 then
   declare -i n=0; for i in $(cat tumor_read_groups.txt); do n+=1; tumor_replace+=" --replace_rg ${i}=ID:T_${n}\tSM:TUMOR\tPL:PLATFORM"; done
   declare -i n=0; for i in $(cat normal_read_groups.txt); do n+=1; normal_replace+=" --replace_rg ${i}=ID:N_${n}\tSM:NORMAL\tPL:PLATFORM"; done
   command="sentieon driver -t ${nt} -r ${reference_fa} ${tumor_replace} -i ${tumor_bam} ${normal_replace} -i ${normal_bam} --algo TNscope --tumor_sample TUMOR --normal_sample NORMAL --dbsnp ${dbsnp} --pon ${pon} output.vcf"
   echo $command
-  $command
 else
   command="sentieon driver -t ${nt} -r ${reference_fa} -i ${tumor_bam} -i ${normal_bam} --algo TNscope --tumor_sample ${tumor_name} --normal_sample ${normal_name} --dbsnp ${dbsnp} --pon ${pon} output.vcf"
   echo $command
-  $command
-fi
-
-
-
+fi || exit 1
 
 # ******************************************
 # 2. Run TNscope
 # ******************************************
 
-sentieon driver -t $nt -r $reference_fa -i $tumor_bam --algo TNscope --tumor_sample $tumor_name output.vcf || exit 1
-
-sentieon driver -t 16 -r ../GAPFIXRDPDK5.fa --replace_rg A00169_41_H2WM5DMXX_2='ID:T_1\tSM:TUMOR\tPL:PLATFORM' -i ../GAPFI2USVS21.bam --replace_rg A00169_41_H2WM5DMXX_2='ID:N\tSM:NORMAL\tPL:PLATFORM' -i ../GAPFI14IJOX2.bam --algo TNscope --tumor_sample TUMOR --normal_sample NORMAL --dbsnp ../GAPFI4LJRN98.vcf.gz --pon panel_of_normals.vcf.gz test_tumor_normal_with_panel.vcf
+$command || exit 1
 
 # ******************************************
 # 3. Compress and index output
